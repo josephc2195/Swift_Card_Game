4ca4ff20548f13891b16f91e08268de1861088f6
Joseph <chicaj1221@gmail.com>
2020-02-16 21:39:30 -0500

    update

--- card_memory/ContentView.swift
+++ card_memory/ContentView.swift
@@ -10,23 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     
+    private var card = ["back", "C"]
+    @State private var numbers = [0, 1]
     @State private var num1 = 2
     @State private var num2 = 5
     @State private var lives = 3
     @State private var height = 1
     @State private var width = 4
+    @State private var flipped = true
     var body: some View {
+        
         ZStack{
+            //background
             Image("green").resizable().edgesIgnoringSafeArea(.all)
             
             VStack{
-                
+                //title
                 Text("TEST YOUR MEMORY")
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .foregroundColor(Color(red: 10.0, green: 40.0, blue: 50.0, opacity: 1.0))
                     .underline()
-                
+                //subtitle
                 HStack(){
                     Text("Player Lives: " + String(lives))
                         .font(.headline)
@@ -44,30 +49,55 @@ struct ContentView: View {
                         .font(.headline)
                         .foregroundColor(Color.white)
                 }
-                .padding(.top, 40.0)
+                .padding([.leading, .trailing], 20).padding(.top, 40)
                 
                 Spacer()
-                
-                HStack{
+                //test if cards are flipped
+                if flipped {
+                    
+                    VStack{
+                        Spacer()
                         
-                    Image(String(num1) + "C").resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
-                    Image(String(num2) + "C").resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
-                    Image(String(num2) + "C").resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
-                    Image(String(num2) + "C").resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                        HStack{
+                            
+                            Image(String(num1) + String(card[numbers[1]])).resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                            Image(String(num2) + String(card[numbers[1]])).resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                            Image(String(num1) + String(card[numbers[1]])).resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                            Image(String(num2) + String(card[numbers[1]])).resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                            
+                        }
+                    
+                        Spacer()
+                        
+                        Button(action: {
+                            self.flipped.toggle()
+                            
+                            
+                        }) {
+                            Text("Flip Cards")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.red)
+                                .buttonStyle(/*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Button Style@*/DefaultButtonStyle()/*@END_MENU_TOKEN@*/)
+                                
+                        }
+                        
+                    }
+                    
+                
+                }
+                else {
+                    HStack{
+                        
+                        Image(card[numbers[0]]).resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                        Image(card[numbers[0]]).resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                        Image(card[numbers[0]]).resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                        Image(card[numbers[0]]).resizable().frame(maxWidth: 50, maxHeight: 75).aspectRatio(contentMode: .fill)
+                    }
+                    Spacer()
                 }
                 
-                Spacer()
                 
-                Button(action: {
-                    self.num1 = Int.random(in: 2...4)
-                    self.num2 = Int.random(in: 2...4)
-                }) {
-                    Text("Flip Cards")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.red)
-                        .buttonStyle(/*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Button Style@*/DefaultButtonStyle()/*@END_MENU_TOKEN@*/)
-                }
             }
         }
         
@@ -82,3 +112,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
